69eb7381a95ba809c08f180ac16c129751f2d21c
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-04-11 20:45:51 -0300

    Add blablabla

--- solucao/solucao/Form1.Designer.cs
+++ solucao/solucao/Form1.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel3 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(linkLabel1);
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(textBox1);
+            panel3.Location = new Point(475, 140);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(350, 450);
+            panel3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.IndianRed;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(28, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "E-mail";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.IndianRed;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.ForeColor = Color.Black;
+            textBox2.Location = new Point(28, 109);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 31);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Senha";
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 7F);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(132, 177);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 25);
+            button1.TabIndex = 2;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 6F);
+            linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(132, 435);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(95, 15);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Recuperar Senha";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(1231, 19);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(65, 52);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImage = Properties.Resources._4k_pc_8dxv1c1kpj1bsvde;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel3);
+            panel1.Location = new Point(0, -7);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1310, 733);
+            panel1.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(1180, 19);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(65, 52);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(135, 152, 34);
+            ClientSize = new Size(1308, 721);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.Desktop;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel3;
+        private LinkLabel linkLabel1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private PictureBox pictureBox2;
     }
 }
--- solucao/solucao/Form1.cs
+++ solucao/solucao/Form1.cs
@@ -6,5 +6,10 @@ namespace solucao
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
